8c3cb2ed103c86443cf6cfca818747c21b323d9b
KnorpelSenf <shtrog@gmail.com>
2021-04-07 23:44:27 +0200

    Export InputFile

--- deno/src/mod.ts
+++ deno/src/mod.ts
@@ -1,8 +1,30 @@
+// Commonly used stuff
+export { BotError, Bot } from './bot.ts'
+export type { PollingOptions, ErrorHandler, BotConfig } from './bot.ts'
+
+export { InputFile } from './platform.ts'
+
+export { Context } from './context.ts'
+
+// Convenience stuff and helpers
 export * from './convenience/keyboard.ts'
 export * from './convenience/router.ts'
 export * from './convenience/session.ts'
 export * from './convenience/webhook.ts'
 
+// A little more advanced stuff
+export { Composer } from './composer.ts'
+export type {
+    NextFunction,
+    Middleware,
+    MiddlewareObj,
+    MiddlewareFn,
+} from './composer.ts'
+
+export { matchFilter } from './filter.ts'
+export type { Filter, FilterQuery } from './filter.ts'
+
+// Internal stuff for expert users
 export { Api } from './core/api.ts'
 export type {
     RawApi,
@@ -13,19 +35,3 @@ export type {
     ApiClientOptions,
 } from './core/client.ts'
 export { GrammyError } from './core/error.ts'
-
-export { BotError, Bot } from './bot.ts'
-export type { PollingOptions, ErrorHandler, BotConfig } from './bot.ts'
-
-export { Composer } from './composer.ts'
-export type {
-    NextFunction,
-    Middleware,
-    MiddlewareObj,
-    MiddlewareFn,
-} from './composer.ts'
-
-export { Context } from './context.ts'
-
-export { matchFilter } from './filter.ts'
-export type { Filter, FilterQuery } from './filter.ts'
