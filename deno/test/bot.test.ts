e6bdff179dd4ca8722d21280e9b9379c58d43617
KnorpelSenf <shtrog@gmail.com>
2021-05-31 21:09:14 +0200

    Update Deno std in tests

--- deno/test/bot.test.ts
+++ deno/test/bot.test.ts
@@ -2,7 +2,7 @@ import { Bot } from '../src/bot.ts'
 import {
     assertThrows,
     assertEquals,
-} from 'https://deno.land/std@0.87.0/testing/asserts.ts'
+} from 'https://deno.land/std@0.97.0/testing/asserts.ts'
 
 function createBot(token = 'fake-token') {
     return new Bot(token)
