d67a35b8e13dcc727c1e41405c7fc1077c7e162d
KnorpelSenf <shtrog@gmail.com>
2021-10-01 13:16:18 +0200

    Revert "Update dependencies"
    This reverts commit 28a37b3fdea513c850163c7852a2406053f44da8.

--- deno/src/core/client.ts
+++ deno/src/core/client.ts
@@ -242,8 +242,7 @@ class ApiClient<R extends RawApi> {
                 }
                 throw new HttpError(msg, err)
             }
-            // deno-lint-ignore no-explicit-any
-            return (await res.json()) as any // node-fetch returns `unknown`
+            return await res.json()
         }
     }
 
