231e54567effd201d5940fa03cc1740076d63ba7
KnorpelSenf <shtrog@gmail.com>
2021-10-25 00:23:43 +0200

    Incorporated updated method docs

--- src/context.ts
+++ src/context.ts
@@ -1449,7 +1449,7 @@ export class Context implements RenamedUpdate {
      * Context-aware alias for `api.answerInlineQuery`. Use this method to send answers to an inline query. On success, True is returned.
      * No more than 50 results per query are allowed.
      *
-     * Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the results, or even before showing any. The user presses the button, switches to a private chat with the bot and, in doing so, passes a start parameter that instructs the bot to return an oauth link. Once done, the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities.
+     * Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the results, or even before showing any. The user presses the button, switches to a private chat with the bot and, in doing so, passes a start parameter that instructs the bot to return an OAuth link. Once done, the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities.
      *
      * @param results An array of results for the inline query
      * @param other Optional remaining parameters, confer the official reference below
--- src/core/api.ts
+++ src/core/api.ts
@@ -172,7 +172,7 @@ export class Api<R extends RawApi = RawApi> {
     }
 
     /**
-     * A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object.
+     * A simple method for testing your bot's authentication token. Requires no parameters. Returns basic information about the bot in form of a User object.
      *
      * @param signal Optional `AbortSignal` to cancel the request
      *
@@ -1645,7 +1645,7 @@ export class Api<R extends RawApi = RawApi> {
      * Use this method to send answers to an inline query. On success, True is returned.
      * No more than 50 results per query are allowed.
      *
-     * Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the results, or even before showing any. The user presses the button, switches to a private chat with the bot and, in doing so, passes a start parameter that instructs the bot to return an oauth link. Once done, the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities.
+     * Example: An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the results, or even before showing any. The user presses the button, switches to a private chat with the bot and, in doing so, passes a start parameter that instructs the bot to return an OAuth link. Once done, the bot can offer a switch_inline button so that the user can easily return to the chat where they wanted to use the bot's inline capabilities.
      *
      * @param inline_query_id Unique identifier for the answered query
      * @param results An array of results for the inline query
