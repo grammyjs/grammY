0ed632f71eaffaaf99e3028d4016c56d1ca351d9
KnightNiwrem <merwinwee@gmail.com>
2021-11-07 23:06:53 +0800

    Run deno fmt again

--- src/composer.ts
+++ src/composer.ts
@@ -1,9 +1,5 @@
 import { Context } from "./context.ts";
-import {
-    Filter,
-    FilterQuery,
-    matchFilter,
-} from "./filter.ts";
+import { Filter, FilterQuery, matchFilter } from "./filter.ts";
 
 type MaybePromise<T> = T | Promise<T>;
 type MaybeArray<T> = T | T[];
