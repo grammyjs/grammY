903f104d99ccadc1212d1d8e477471795c0d1abc
KnorpelSenf <shtrog@gmail.com>
2021-11-25 00:22:39 +0100

    Yet another fix

--- bundle-es6.sh
+++ bundle-es6.sh
@@ -2,6 +2,11 @@
 
 if [ -n $INSTALL_DENO ]; then
     curl -fsSL https://deno.land/x/install/install.sh | sh
+    export DENO_INSTALL="/opt/buildhome/.deno"
+    export PATH="$DENO_INSTALL/bin:$PATH"
 fi
 
 deno --version
+
+mkdir bundles
+echo "hello" >bundles/test.txt
