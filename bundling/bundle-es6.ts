b24f770764453c4d14b7ef112a9cdcb4da93de45
KnorpelSenf <shtrog@gmail.com>
2021-11-30 16:36:22 +0100

    ES6 bundling (#111)

---
+++ bundling/bundle-es6.ts
@@ -0,0 +1,59 @@
+console.log("ES6 bundling script started.");
+
+// === THROTTLING UTILS
+const CONCURRENCY = 4;
+async function throttle(tasks: Array<() => Promise<void>>) {
+    const workers = Array<Promise<void>>(CONCURRENCY).fill(Promise.resolve());
+    for (let i = 0; i < tasks.length; i++) {
+        const w = i % workers.length;
+        const task = tasks[i];
+        workers[w] = workers[w].then(task);
+    }
+    await Promise.all(workers);
+}
+
+// === LISTING VERSIONS
+console.log("Fetching release list ...");
+const res = await fetch("https://cdn.deno.land/grammy/meta/versions.json");
+const { versions: vs } = await res.json();
+const versions: string[] = vs.filter((v: string) => v.startsWith("v1"));
+console.log("Found", versions.length, "stable releases");
+console.log(versions);
+
+function url(version: string) {
+    return `https://deno.land/x/grammy@${version}/mod.ts`;
+}
+
+// === CACHING
+async function cache(url: string) {
+    console.log(url);
+    await Deno.run({ cmd: ["deno", "cache", "--quiet", url] }).status();
+}
+
+console.log("Caching source code and releases ...");
+await cache("../src/mod.ts");
+await throttle(versions.map(url).map((v: string) => () => cache(v)));
+
+// === BUNDLING
+async function createBundle(source: string, release: string) {
+    console.log(release);
+    const bundled = await Deno.emit(source, { bundle: "module" });
+    const bundledCode = bundled.files["deno:///bundle.js"];
+    const transpiled = await Deno.emit("/src.ts", {
+        sources: { "/src.ts": bundledCode },
+        compilerOptions: { target: "es6" },
+    });
+    const transpiledCode = transpiled.files["file:///src.ts.js"];
+    const path = `./bundles/es6@${release}.js`;
+    await Deno.writeTextFile(path, transpiledCode);
+}
+
+console.log("Bundling source and releases ...");
+await Deno.mkdir("./bundles/", { recursive: true });
+await createBundle("../src/mod.ts", "dev");
+await throttle(versions.map((v: string) => () => createBundle(url(v), v)));
+console.log("Done! Created files:");
+for await (const path of Deno.readDir("./bundles/")) {
+    console.log(path.name);
+}
+console.log("ES6 bundling script complete.");
