1ddcdac5eb50fd3b9e378f904695a84935da49c4
WingLim <643089849@qq.com>
2022-05-20 15:42:52 +0800

    feat: implement `fromType`

--- src/convenience/builders/input_media.ts
+++ src/convenience/builders/input_media.ts
@@ -7,23 +7,33 @@ import {
     type InputMediaVideo,
 } from "../../platform.deno.ts";
 
-export class InputMediaBuilder {
+class GenericInputMediaBuilder<T extends InputMedia = InputMedia> {
     /**
      * Initialize a new `InputMediaBuilder` with optional `InputMedia` array.
      *
      * @param media The media to initialize the builder with
      */
-    constructor(public readonly media: InputMedia[] = []) {
+    constructor(public readonly media: T[] = []) {
     }
     /**
      * Allows you to add a `InputMedia`.
      *
      * @param media The media to add
      */
-    add(...media: InputMedia[]) {
+    add(...media: T[]) {
         this.media.push(...media);
         return this;
     }
+    /**
+     * Return an array of `InputMedia` objects. You can use it
+     * in `sendMediaGroup`.
+     */
+    build() {
+        return this.media;
+    }
+}
+
+export class InputMediaBuilder extends GenericInputMediaBuilder {
     /**
      * Add a `InputMediaPhoto`.
      *
@@ -65,18 +75,20 @@ export class InputMediaBuilder {
         return this.add({ type: "document", ...document });
     }
     /**
-     * Return an array of `InputMedia` objects. You can use it
-     * in `sendMediaGroup`.
-     */
-    build() {
-        return this.media;
-    }
-    /**
      * Staic method to create an `InputMediaBuilder`.
      *
      * @param media The media to add
      */
     static from(media: InputMedia[]) {
         return new InputMediaBuilder(media);
+    }
+    /**
+     * Static method to create an `InputMediaBuilder` from a `type`.
+     *
+     * @param media The media to add
+     * @param type The type of media
+     */
+    static fromType<T extends InputMedia = InputMedia>(media: Omit<T, "type">[], type: T["type"]) {
+        return new GenericInputMediaBuilder<T>(media.map((m) => ({ ...m, type } as T)));
     }
 }
