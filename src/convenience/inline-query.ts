0165e719162764a84b0b3ad43c1a70c9e81aedde
Shane Avery Sistoza <me@ssistoza.dev>
2022-07-28 17:06:57 -0400

    add(inline-query-result-builder): initial pass on creating the builder

---
+++ src/convenience/inline-query.ts
@@ -0,0 +1,209 @@
+import {
+    type InlineQueryResultDocument,
+    type InlineQueryResultCachedDocument,
+    type InlineQueryResult,
+    type InlineQueryResultArticle,
+    type InlineQueryResultAudio,
+    type InlineQueryResultContact,
+    type InlineQueryResultCachedAudio,
+    type InlineQueryResultVoice,
+    type InlineQueryResultGame,
+    type InlineQueryResultGif,
+    type InlineQueryResultCachedGif,
+    type InlineQueryResultLocation,
+    type InlineQueryResultCachedMpeg4Gif,
+    type InlineQueryResultMpeg4Gif,
+    type InlineQueryResultCachedPhoto,
+    type InlineQueryResultPhoto,
+    type InlineQueryResultVenue,
+    type InlineQueryResultCachedVideo,
+    type InlineQueryResultVideo,
+    type InlineQueryResultCachedSticker,
+    type InlineQueryResultCachedVoice,
+} from "../platform.deno.ts";
+
+type WithoutType<T extends InlineQueryResult> = Omit<T, "type">;
+
+type QueryResult<
+    Cached extends boolean,
+    CachedResult extends InlineQueryResult,
+    Result extends InlineQueryResult
+> = Cached extends true ? WithoutType<CachedResult> : WithoutType<Result>;
+
+export class InlineQueryResultBuilder {
+    constructor(
+        private readonly inline_query_results: Array<InlineQueryResult> = []
+    ) {}
+
+    add(query: InlineQueryResult) {
+        this.inline_query_results.push(query);
+        return this;
+    }
+
+    addMany(...query: Array<InlineQueryResult>) {
+        this.inline_query_results.push(...query);
+        return this;
+    }
+
+    article(options: WithoutType<InlineQueryResultArticle>) {
+        return this.add({
+            type: "article",
+            ...options,
+        });
+    }
+
+    audio<Cached extends boolean = false>(
+        options: QueryResult<
+            Cached,
+            InlineQueryResultCachedAudio,
+            InlineQueryResultAudio
+        >
+    ) {
+        return this.add({
+            type: "audio",
+            ...options,
+        });
+    }
+
+    audioCache(options: WithoutType<InlineQueryResultCachedAudio>) {
+        return this.audio<true>(options);
+    }
+
+    build() {
+        return this.inline_query_results;
+    }
+
+    contact(options: WithoutType<InlineQueryResultContact>) {
+        return this.add({
+            type: "contact",
+            ...options,
+        });
+    }
+
+    document<Cached extends boolean = false>(
+        options: QueryResult<
+            Cached,
+            InlineQueryResultCachedDocument,
+            InlineQueryResultDocument
+        >
+    ) {
+        return this.add({
+            type: "document",
+            ...options,
+        });
+    }
+
+    documentCache(options: WithoutType<InlineQueryResultCachedDocument>) {
+        return this.document<true>(options);
+    }
+
+    game(options: WithoutType<InlineQueryResultGame>) {
+        return this.add({
+            type: "game",
+            ...options,
+        });
+    }
+
+    gif<Cached extends boolean = false>(
+        options: QueryResult<
+            Cached,
+            InlineQueryResultCachedGif,
+            InlineQueryResultGif
+        >
+    ) {
+        return this.add({
+            type: "gif",
+            ...options,
+        });
+    }
+
+    gifCache(options: WithoutType<InlineQueryResultCachedGif>) {
+        return this.gif<true>(options);
+    }
+
+    location(options: WithoutType<InlineQueryResultLocation>) {
+        return this.add({
+            type: "location",
+            ...options,
+        });
+    }
+
+    mpeg4gif<Cached extends boolean = false>(
+        options: QueryResult<
+            Cached,
+            InlineQueryResultCachedMpeg4Gif,
+            InlineQueryResultMpeg4Gif
+        >
+    ) {
+        return this.add({
+            type: "mpeg4_gif",
+            ...options,
+        });
+    }
+
+    mpeg4gifCache(options: WithoutType<InlineQueryResultCachedMpeg4Gif>) {
+        return this.mpeg4gif<true>(options);
+    }
+
+    photo<Cached extends boolean = false>(
+        options: QueryResult<
+            Cached,
+            InlineQueryResultCachedPhoto,
+            InlineQueryResultPhoto
+        >
+    ) {
+        return this.add({
+            type: "photo",
+            ...options,
+        });
+    }
+
+    photoCache(options: WithoutType<InlineQueryResultCachedPhoto>) {
+        return this.photo<true>(options);
+    }
+
+    venue(options: WithoutType<InlineQueryResultVenue>) {
+        return this.add({ type: "venue", ...options });
+    }
+
+    video<Cached extends boolean = false>(
+        options: QueryResult<
+            Cached,
+            InlineQueryResultCachedVideo,
+            InlineQueryResultVideo
+        >
+    ) {
+        return this.add({
+            type: "video",
+            ...options,
+        });
+    }
+
+    videoCache(options: WithoutType<InlineQueryResultCachedVideo>) {
+        return this.video<true>(options);
+    }
+
+    stickerCache(options: WithoutType<InlineQueryResultCachedSticker>) {
+        return this.add({
+            type: "sticker",
+            ...options,
+        });
+    }
+
+    voice<Cached extends boolean = false>(
+        options: QueryResult<
+            Cached,
+            InlineQueryResultCachedVoice,
+            InlineQueryResultVoice
+        >
+    ) {
+        return this.add({
+            type: "voice",
+            ...options,
+        });
+    }
+
+    voiceCache(options: WithoutType<InlineQueryResultCachedVoice>) {
+        return this.voice<true>(options);
+    }
+}
