895f60ec598ae3efde96859bb63bce7511c35fba
Loskir <kl@sinker.ru>
2022-08-25 16:45:23 +0300

    Fix issue with ctx.has

--- src/filter.ts
+++ src/filter.ts
@@ -433,6 +433,9 @@ type Combine<U, K extends string> = U extends unknown
     ? U & Partial<Record<Exclude<K, keyof U>, undefined>>
     : never;
 
+
+export type FilterE<Q extends FilterQuery> = PerformQueryE<RunQuery<ExpandShortcuts<Q>>>
+
 /**
  * This type infers which properties will be present on the given context object
  * provided it matches the given filter query. If the filter query is a union
@@ -446,16 +449,23 @@ export type Filter<C extends Context, Q extends FilterQuery> = PerformQuery<
     C,
     RunQuery<ExpandShortcuts<Q>>
 >;
+type PerformQueryE<U extends SomeObject> = U extends unknown
+    ? FilteredContextE<Update & U>
+    : never;
 // apply a query result by intersecting it with Update, and then injecting into C
 type PerformQuery<C extends Context, U extends SomeObject> = U extends unknown
     ? FilteredContext<C, Update & U>
     : never;
-// set the given update into a given context object, and adjust the aliases
-type FilteredContext<C extends Context, U extends Update> =
-    & C
+
+type FilteredContextE<U extends Update> =
     & Record<"update", U>
     & AliasProps<Omit<U, "update_id">>
     & Shortcuts<U>;
+
+// set the given update into a given context object, and adjust the aliases
+type FilteredContext<C extends Context, U extends Update> =
+    & C
+    & FilteredContextE<U>;
 
 // helper type to infer shortcuts on context object based on present properties, must be in sync with shortcut impl!
 interface Shortcuts<U extends Update> {
