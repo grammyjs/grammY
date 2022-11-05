51457fd574c5600525f8a871d8801b7018b689f7
KnorpelSenf <shtrog@gmail.com>
2022-11-05 14:32:52 +0100

    Update to Bot API 6.3

--- src/types.deno.ts
+++ src/types.deno.ts
@@ -1,13 +1,13 @@
 // === Needed imports
-import { basename } from "https://deno.land/std@0.153.0/path/mod.ts";
-import { iterateReader } from "https://deno.land/std@0.153.0/streams/mod.ts";
-import { type InputFileProxy } from "https://esm.sh/@grammyjs/types@2.9.1";
+import { basename } from "https://deno.land/std@0.162.0/path/mod.ts";
+import { iterateReader } from "https://deno.land/std@0.162.0/streams/mod.ts";
+import { type InputFileProxy } from "https://esm.sh/@grammyjs/types@2.10.0";
 import { debug as d, isDeno, toRaw } from "./platform.deno.ts";
 
 const debug = d("grammy:warn");
 
 // === Export all API types
-export * from "https://esm.sh/@grammyjs/types@2.9.1";
+export * from "https://esm.sh/@grammyjs/types@2.10.0";
 
 /** Something that looks like a URL. */
 interface URLLike {
