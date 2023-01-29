583635fef1c961e6556d56f59d77e52dc297ab88
KnorpelSenf <shtrog@gmail.com>
2023-01-29 18:54:34 +0100

    feat: add Cloudflare Workers adapter for native modules

--- src/convenience/frameworks.web.ts
+++ src/convenience/frameworks.web.ts
@@ -1,7 +1,7 @@
 // deno-lint-ignore-file no-explicit-any
 import { SECRET_HEADER } from "./frameworks.shared.ts";
 
-/** Native CloudFlare workers */
+/** Native CloudFlare workers (service worker) */
 const cloudflare = (
     event: { request: Request; respondWith: (res: Promise<Response>) => void },
 ) => {
@@ -34,6 +34,37 @@ const cloudflare = (
     };
 };
 
+/** Native CloudFlare workers (module worker) */
+const cloudflare_module = (
+    request: Request
+) => {
+    let resolveResponse: (res: Response) => void;
+    return {
+        update: request.json(),
+        header: request.headers.get(SECRET_HEADER) || undefined,
+        end: () => {
+            resolveResponse(new Response(null, { status: 200 }));
+        },
+        respond: (json: string) => {
+            const res = new Response(json, {
+                status: 200,
+                headers: { "Content-Type": "application/json" },
+            });
+            resolveResponse(res);
+        },
+        unauthorized: () => {
+            const res = new Response('"unauthorized"', {
+                status: 401,
+                statusText: "secret token is wrong",
+            });
+            resolveResponse(res);
+        },
+        handlerReturn: new Promise<Response>((resolve) => {
+            resolveResponse = resolve;
+        })
+    };
+};
+
 /** worktop CloudFlare workers framework */
 const worktop = (req: any, res: any) => ({
     update: Promise.resolve(req.body.json()),
@@ -43,4 +74,4 @@ const worktop = (req: any, res: any) => ({
     unauthorized: () => res.send(401, "secret token is wrong"),
 });
 
-export const adapters = { cloudflare, worktop };
+export const adapters = { cloudflare, "cloudflare-mod": cloudflare_module, worktop };
