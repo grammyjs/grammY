3daab9b432c5a7063121cb4342d368b7ac651070
KnorpelSenf <shtrog@gmail.com>
2023-03-10 18:20:17 +0100

    deps: update types for Deno and web builds, too

--- src/types.web.ts
+++ src/types.web.ts
@@ -10,10 +10,10 @@ import {
     type InputMediaVideo as InputMediaVideoF,
     type InputSticker as InputStickerF,
     type Opts as OptsF,
-} from "https://deno.land/x/grammy_types@v3.0.0/mod.ts";
+} from "https://deno.land/x/grammy_types@v3.0.1/mod.ts";
 
 // === Export all API types
-export * from "https://deno.land/x/grammy_types@v3.0.0/mod.ts";
+export * from "https://deno.land/x/grammy_types@v3.0.1/mod.ts";
 
 /** Something that looks like a URL. */
 interface URLLike {
