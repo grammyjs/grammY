5c77a00a12687d32e7c3cae491db5ff47c6fd142
KnorpelSenf <shtrog@gmail.com>
2023-03-11 22:57:28 +0100

    fix: type of createNewStickerSet > stickers is an array (#392)

--- src/types.deno.ts
+++ src/types.deno.ts
@@ -11,13 +11,13 @@ import {
     type InputMediaVideo as InputMediaVideoF,
     type InputSticker as InputStickerF,
     type Opts as OptsF,
-} from "https://deno.land/x/grammy_types@v3.0.1/mod.ts";
+} from "https://deno.land/x/grammy_types@v3.0.2/mod.ts";
 import { debug as d, isDeno } from "./platform.deno.ts";
 
 const debug = d("grammy:warn");
 
 // === Export all API types
-export * from "https://deno.land/x/grammy_types@v3.0.1/mod.ts";
+export * from "https://deno.land/x/grammy_types@v3.0.2/mod.ts";
 
 /** Something that looks like a URL. */
 interface URLLike {
--- src/types.web.ts
+++ src/types.web.ts
@@ -10,10 +10,10 @@ import {
     type InputMediaVideo as InputMediaVideoF,
     type InputSticker as InputStickerF,
     type Opts as OptsF,
-} from "https://deno.land/x/grammy_types@v3.0.1/mod.ts";
+} from "https://deno.land/x/grammy_types@v3.0.2/mod.ts";
 
 // === Export all API types
-export * from "https://deno.land/x/grammy_types@v3.0.1/mod.ts";
+export * from "https://deno.land/x/grammy_types@v3.0.2/mod.ts";
 
 /** Something that looks like a URL. */
 interface URLLike {
