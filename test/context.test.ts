07fdf29e90be8c29fbe745dc920106f0501d4267
KnorpelSenf <shtrog@gmail.com>
2023-03-12 01:10:13 +0100

    Revert "chore: test CI coverage tool"
    This reverts commit be4e25a702a1af2a651ad4ade13cbcd2d4fb648f.

---
+++ test/context.test.ts
@@ -0,0 +1,400 @@
+import { Context } from "../src/context.ts";
+import { Api } from "../src/mod.ts";
+import {
+    type Chat,
+    type Message,
+    type Update,
+    type User,
+    type UserFromGetMe,
+} from "../src/types.ts";
+import {
+    assert,
+    assertEquals,
+    assertFalse,
+    assertThrows,
+} from "https://deno.land/std@0.178.0/testing/asserts.ts";
+import { describe, it } from "https://deno.land/std@0.178.0/testing/bdd.ts";
+
+describe("Context", () => {
+    const u = { id: 42, first_name: "bot", is_bot: true } as User;
+    const c = { id: 100, type: "private" } as Chat;
+    const m = { text: "a", from: u, chat: c, sender_chat: c } as Message;
+    const update = {
+        message: m,
+        edited_message: m,
+        channel_post: m,
+        edited_channel_post: m,
+        inline_query: { id: "b", from: u, query: "iq" },
+        chosen_inline_result: { from: u, inline_message_id: "x" },
+        callback_query: {
+            data: "cb",
+            game_short_name: "game",
+            message: m,
+            from: u,
+            inline_message_id: "y",
+        },
+        shipping_query: { id: "d", from: u },
+        pre_checkout_query: { id: "e", from: u },
+        poll: { id: "f" },
+        poll_answer: { poll_id: "g" },
+        my_chat_member: { date: 1, from: u, chat: c },
+        chat_member: { date: 2, from: u, chat: c },
+        chat_join_request: { date: 3, from: u, chat: c },
+    } as Update;
+    const api = new Api("dummy-token");
+    const me = { id: 42 } as UserFromGetMe;
+
+    it("provide basic properties", () => {
+        const ctx = new Context(update, api, me);
+        assertEquals(ctx.update, update);
+        assertEquals(ctx.api, api);
+        assertEquals(ctx.me, me);
+    });
+
+    it("should have aliases", () => {
+        const ctx = new Context(update, api, me);
+        assertEquals(ctx.message, update.message);
+        assertEquals(ctx.editedMessage, update.edited_message);
+        assertEquals(ctx.channelPost, update.channel_post);
+        assertEquals(ctx.editedChannelPost, update.edited_channel_post);
+        assertEquals(ctx.inlineQuery, update.inline_query);
+        assertEquals(ctx.chosenInlineResult, update.chosen_inline_result);
+        assertEquals(ctx.callbackQuery, update.callback_query);
+        assertEquals(ctx.shippingQuery, update.shipping_query);
+        assertEquals(ctx.preCheckoutQuery, update.pre_checkout_query);
+        assertEquals(ctx.poll, update.poll);
+        assertEquals(ctx.pollAnswer, update.poll_answer);
+        assertEquals(ctx.myChatMember, update.my_chat_member);
+        assertEquals(ctx.chatMember, update.chat_member);
+        assertEquals(ctx.chatJoinRequest, update.chat_join_request);
+    });
+
+    it("should have update shortcuts", () => {
+        const ctx = new Context(update, api, me);
+        assertEquals(ctx.message, update.message);
+        assertEquals(ctx.editedMessage, update.edited_message);
+        assertEquals(ctx.channelPost, update.channel_post);
+        assertEquals(ctx.editedChannelPost, update.edited_channel_post);
+        assertEquals(ctx.chosenInlineResult, update.chosen_inline_result);
+        assertEquals(ctx.callbackQuery, update.callback_query);
+        assertEquals(ctx.shippingQuery, update.shipping_query);
+        assertEquals(ctx.preCheckoutQuery, update.pre_checkout_query);
+        assertEquals(ctx.poll, update.poll);
+        assertEquals(ctx.pollAnswer, update.poll_answer);
+        assertEquals(ctx.myChatMember, update.my_chat_member);
+        assertEquals(ctx.chatMember, update.chat_member);
+        assertEquals(ctx.chatJoinRequest, update.chat_join_request);
+    });
+
+    it(".msg should aggregate messages", () => {
+        let up: Update, ctx: Context;
+
+        up = { message: update.message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.msg, up.message);
+        up = { edited_message: update.edited_message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.msg, up.edited_message);
+        up = { callback_query: update.callback_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.msg, up.callback_query?.message);
+        up = { channel_post: update.channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.msg, up.channel_post);
+        up = { edited_channel_post: update.edited_channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.msg, up.edited_channel_post);
+    });
+
+    it(".chat should aggregate chats", () => {
+        let up: Update, ctx: Context;
+
+        up = { message: update.message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.message?.chat);
+        up = { edited_message: update.edited_message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.edited_message?.chat);
+        up = { callback_query: update.callback_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.callback_query?.message?.chat);
+        up = { channel_post: update.channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.channel_post?.chat);
+        up = { edited_channel_post: update.edited_channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.edited_channel_post?.chat);
+        up = { my_chat_member: update.my_chat_member } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.my_chat_member?.chat);
+        up = { chat_member: update.chat_member } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.chat_member?.chat);
+        up = { chat_join_request: update.chat_join_request } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.chat, up.chat_join_request?.chat);
+    });
+
+    it(".senderChat should aggregate sender chats", () => {
+        let up: Update, ctx: Context;
+
+        up = { message: update.message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.senderChat, up.message?.sender_chat);
+        up = { edited_message: update.edited_message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.senderChat, up.edited_message?.sender_chat);
+        up = { callback_query: update.callback_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.senderChat, up.callback_query?.message?.sender_chat);
+        up = { channel_post: update.channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.senderChat, up.channel_post?.sender_chat);
+        up = { edited_channel_post: update.edited_channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.senderChat, up.edited_channel_post?.sender_chat);
+    });
+
+    it(".from should aggregate user objects", () => {
+        let up: Update, ctx: Context;
+
+        up = { callback_query: update.callback_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.callback_query?.from);
+        up = { inline_query: update.inline_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.inline_query?.from);
+        up = { shipping_query: update.shipping_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.shipping_query?.from);
+        up = { pre_checkout_query: update.pre_checkout_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.pre_checkout_query?.from);
+        up = { chosen_inline_result: update.chosen_inline_result } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.chosen_inline_result?.from);
+        up = { message: update.message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.message?.from);
+        up = { edited_message: update.edited_message } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.edited_message?.from);
+        up = { callback_query: update.callback_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.callback_query?.message?.from);
+        up = { channel_post: update.channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.channel_post?.from);
+        up = { edited_channel_post: update.edited_channel_post } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.edited_channel_post?.from);
+        up = { my_chat_member: update.my_chat_member } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.my_chat_member?.from);
+        up = { chat_member: update.chat_member } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.chat_member?.from);
+        up = { chat_join_request: update.chat_join_request } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.from, up.chat_join_request?.from);
+    });
+
+    it(".inlineMessageId should aggregate inline message identifiers", () => {
+        let up: Update, ctx: Context;
+
+        up = { callback_query: update.callback_query } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.inlineMessageId, up.callback_query?.inline_message_id);
+        up = { chosen_inline_result: update.chosen_inline_result } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(
+            ctx.inlineMessageId,
+            up.chosen_inline_result?.inline_message_id,
+        );
+    });
+
+    it("should be able to check for callback queries", () => {
+        const ctx = new Context(update, api, me);
+
+        assert(Context.has.callbackQuery("cb")(ctx));
+        assert(ctx.hasCallbackQuery("cb"));
+        assert(Context.has.callbackQuery(/^c./)(ctx));
+        assertEquals(ctx.match, "cb".match(/^c./));
+        assert(ctx.hasCallbackQuery(/^c./));
+        assertFalse(Context.has.callbackQuery("bb")(ctx));
+        assertFalse(ctx.hasCallbackQuery("bb"));
+    });
+
+    it("should be able to check for chat types", () => {
+        const ctx = new Context(update, api, me);
+
+        assert(Context.has.chatType("private")(ctx));
+        assert(ctx.hasChatType("private"));
+        assert(Context.has.chatType(["channel", "private"])(ctx));
+        assert(ctx.hasChatType(["channel", "private"]));
+        assertFalse(Context.has.chatType("group")(ctx));
+        assertFalse(ctx.hasChatType("group"));
+    });
+
+    it("should be able to check for commands", () => {
+        let up = {
+            message: {
+                text: "/start args",
+                entities: [{
+                    type: "bot_command",
+                    offset: 0,
+                    length: "/start".length,
+                }],
+            },
+        } as Update;
+        let ctx = new Context(up, api, me);
+
+        assert(Context.has.command("start")(ctx));
+        assert(ctx.hasCommand("start"));
+        assert(Context.has.command(["help", "start"])(ctx));
+        assert(ctx.hasCommand(["help", "start"]));
+        assertEquals(ctx.match, "args");
+        assertFalse(Context.has.command("help")(ctx));
+        assertFalse(ctx.hasCommand("help"));
+
+        assertThrows(
+            () => Context.has.command("/asdf"),
+            "Do not include '/' when registering command handlers (use 'asdf' not '/asdf')",
+        );
+
+        up = {
+            message: {
+                text: "Test with /start args",
+                entities: [{
+                    type: "bot_command",
+                    offset: "Test with ".length,
+                    length: "/start".length,
+                }],
+            },
+        } as Update;
+        ctx = new Context(up, api, me);
+        assertFalse(Context.has.command("start")(ctx));
+        assertFalse(ctx.hasCommand("start"));
+
+        up = { message: { text: "/start" } } as Update;
+        ctx = new Context(up, api, me);
+        assertFalse(Context.has.command("start")(ctx));
+        assertFalse(ctx.hasCommand("start"));
+    });
+
+    it("should be able to check for game queries", () => {
+        const ctx = new Context(update, api, me);
+
+        assert(Context.has.gameQuery("game")(ctx));
+        assert(ctx.hasGameQuery("game"));
+        assert(Context.has.gameQuery(/^ga..$/)(ctx));
+        assertEquals(ctx.match, "game".match(/^ga..$/));
+        assert(ctx.hasGameQuery(/^ga..$/));
+        assertFalse(Context.has.gameQuery("xy")(ctx));
+        assertFalse(ctx.hasGameQuery("xy"));
+    });
+
+    it("should be able to check for inline queries", () => {
+        const ctx = new Context(update, api, me);
+
+        assert(Context.has.inlineQuery("iq")(ctx));
+        assert(ctx.hasInlineQuery("iq"));
+        assert(Context.has.inlineQuery(/^i./)(ctx));
+        assertEquals(ctx.match, "iq".match(/^i./));
+        assert(ctx.hasInlineQuery(/^i./));
+        assertFalse(Context.has.inlineQuery("cb")(ctx));
+        assertFalse(ctx.hasInlineQuery("cb"));
+    });
+
+    it("should be able to match filter queries", () => {
+        const ctx = new Context(update, api, me);
+
+        assert(Context.has.filterQuery(":text")(ctx));
+        assert(ctx.has(":text"));
+        assertFalse(Context.has.filterQuery(":photo")(ctx));
+        assertFalse(ctx.has(":photo"));
+    });
+
+    it("should be able to check for text", () => {
+        const ctx = new Context(update, api, me);
+
+        assert(Context.has.text("a")(ctx));
+        assert(ctx.hasText("a"));
+        assertFalse(Context.has.text("b")(ctx));
+        assertFalse(ctx.hasText("b"));
+    });
+
+    it("should be able to extract entity text", () => {
+        let up = {
+            message: {
+                text: "/start some@email.com",
+                entities: [
+                    {
+                        type: "bot_command",
+                        offset: 0,
+                        length: "/start".length,
+                    },
+                    {
+                        type: "email",
+                        offset: "/start ".length,
+                        length: "some@email.com".length,
+                    },
+                ],
+            },
+        } as Update;
+        let ctx = new Context(up, api, me);
+
+        assertEquals(ctx.entities(), [
+            {
+                type: "bot_command",
+                offset: 0,
+                length: "/start".length,
+                text: "/start",
+            },
+            {
+                type: "email",
+                offset: "/start ".length,
+                length: "some@email.com".length,
+                text: "some@email.com",
+            },
+        ]);
+        assertEquals(ctx.entities("email"), [{
+            ...up.message!.entities![1],
+            text: "some@email.com",
+        }]);
+
+        up = {
+            message: {
+                text: "/start some@email.com",
+            },
+        } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.entities(), []);
+
+        up = { message: {} } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.entities(), []);
+
+        up = {} as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.entities(), []);
+
+        up = {
+            message: {
+                caption: "some@email.com",
+                caption_entities: [{
+                    type: "email",
+                    offset: 0,
+                    length: "some@email.com".length,
+                }],
+            },
+        } as Update;
+        ctx = new Context(up, api, me);
+        assertEquals(ctx.entities(), [{
+            type: "email",
+            offset: 0,
+            length: "some@email.com".length,
+            text: "some@email.com",
+        }]);
+    });
+});
---
+++ test/convenience/keyboard.test.ts
@@ -0,0 +1,130 @@
+import { InlineKeyboard, Keyboard } from "../../src/convenience/keyboard.ts";
+import { type LoginUrl } from "../../src/types.ts";
+import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
+import { describe, it } from "https://deno.land/std@0.178.0/testing/bdd.ts";
+
+describe("Keyboard", () => {
+    it("should take initial buttons", () => {
+        const keyboard = new Keyboard([[{ text: "button" }]]);
+        assertEquals(keyboard.build(), [[{ text: "button" }]]);
+    });
+
+    it("should create rows and columns", () => {
+        const keyboard = new Keyboard([[{ text: "0" }]])
+            .add({ text: "1" }, { text: "2" }).row()
+            .add({ text: "3" }, { text: "4" }).add({ text: "5" });
+        assertEquals(keyboard.build(), [
+            [{ text: "0" }, { text: "1" }, { text: "2" }],
+            [{ text: "3" }, { text: "4" }, { text: "5" }],
+        ]);
+    });
+
+    it("should support different buttons", () => {
+        const keyboard = new Keyboard()
+            .text("button")
+            .requestContact("contact")
+            .requestLocation("location")
+            .requestPoll("poll", "quiz")
+            .webApp("web app", "https://grammy.dev")
+            .requestUser("user", 12, { user_is_bot: true })
+            .requestChat("chat", 42);
+        assertEquals(keyboard.build(), [[
+            { text: "button" },
+            { text: "contact", request_contact: true },
+            { text: "location", request_location: true },
+            { text: "poll", request_poll: { type: "quiz" } },
+            { text: "web app", web_app: { url: "https://grammy.dev" } },
+            {
+                text: "user",
+                request_user: { request_id: 12, user_is_bot: true },
+            },
+            {
+                text: "chat",
+                request_chat: { request_id: 42, chat_is_channel: false },
+            },
+        ]]);
+    });
+
+    it("should support reply markup options", () => {
+        const keyboard = new Keyboard();
+        assertEquals(keyboard.is_persistent, undefined);
+        assertEquals(keyboard.selective, undefined);
+        assertEquals(keyboard.one_time_keyboard, undefined);
+        assertEquals(keyboard.resize_keyboard, undefined);
+        assertEquals(keyboard.input_field_placeholder, undefined);
+        keyboard
+            .persistent()
+            .selected(false)
+            .oneTime(true)
+            .resized(false)
+            .placeholder("placeholder");
+        assertEquals(keyboard.is_persistent, true);
+        assertEquals(keyboard.selective, false);
+        assertEquals(keyboard.one_time_keyboard, true);
+        assertEquals(keyboard.resize_keyboard, false);
+        assertEquals(keyboard.input_field_placeholder, "placeholder");
+    });
+});
+
+describe("InlineKeyboard", () => {
+    const btn = { text: "text", callback_data: "data" };
+
+    it("should take initial buttons", () => {
+        const keyboard = new InlineKeyboard([[btn], [btn, btn]]);
+        assertEquals(keyboard.inline_keyboard, [[btn], [btn, btn]]);
+    });
+
+    it("should create rows and columns", () => {
+        const keyboard = new InlineKeyboard([[btn]])
+            .add(btn, btn).row()
+            .add(btn, btn).add(btn);
+        assertEquals(keyboard.inline_keyboard, [
+            [btn, btn, btn],
+            [btn, btn, btn],
+        ]);
+    });
+
+    it("should support different buttons", () => {
+        const url: LoginUrl = {
+            url: "https://grammy.dev",
+            forward_text: "forward",
+            bot_username: "bot",
+            request_write_access: true,
+        };
+        const keyboard = new InlineKeyboard()
+            .url("url", "https://grammy.dev")
+            .text("button")
+            .text("button", "data")
+            .webApp("web app", "https://grammy.dev")
+            .login("login", "https://grammy.dev")
+            .login("login", url)
+            .switchInline("inline")
+            .switchInline("inline", "query")
+            .switchInlineCurrent("inline current")
+            .switchInlineCurrent("inline current", "query")
+            .game("game")
+            .pay("pay");
+        assertEquals(keyboard.inline_keyboard, [
+            [
+                { text: "url", url: "https://grammy.dev" },
+                { text: "button", callback_data: "button" },
+                { text: "button", callback_data: "data" },
+                { text: "web app", web_app: { url: "https://grammy.dev" } },
+                { text: "login", login_url: { url: "https://grammy.dev" } },
+                { text: "login", login_url: url },
+                { text: "inline", switch_inline_query: "" },
+                { text: "inline", switch_inline_query: "query" },
+                {
+                    switch_inline_query_current_chat: "",
+                    text: "inline current",
+                },
+                {
+                    switch_inline_query_current_chat: "query",
+                    text: "inline current",
+                },
+                { text: "game", callback_game: {} },
+                { text: "pay", pay: true },
+            ],
+        ]);
+    });
+});
