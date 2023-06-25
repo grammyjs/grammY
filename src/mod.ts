7a770300ec874d8f7953cd3112d49c261161de5e
KnorpelSenf <shtrog@gmail.com>
2023-06-26 00:33:43 +0200

    feat: add API_CONSTANTS (#423)

--- src/mod.ts
+++ src/mod.ts
@@ -20,6 +20,7 @@ export {
 } from "./context.ts";
 
 // Convenience stuff, built-in plugins, and helpers
+export * from "./convenience/constants.ts";
 export * from "./convenience/inline_query.ts";
 export * from "./convenience/keyboard.ts";
 export * from "./convenience/session.ts";
