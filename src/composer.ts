11a6b13404f06c4582cdc8a0781dbd649dae9a4b
KnorpelSenf <shtrog@gmail.com>
2023-06-26 23:37:52 +0200

    Revert "refactor: avoid top-level await (#436)"
    This reverts commit 0b5cb51275ecc8ce42eef3dc6fc1d9c861e71f80.

--- src/composer.ts
+++ src/composer.ts
@@ -332,11 +332,11 @@ export class Composer<C extends Context> implements MiddlewareObj<C> {
      * via `ctx.match`.
      *
      * > **Did you know?** You can use deep linking
-     * > (https://core.telegram.org/bots/features#deep-linking) to let users
-     * > start your bot with a custom payload. As an example, send someone the
-     * > link https://t.me/name-of-your-bot?start=custom-payload and register a
-     * > start command handler on your bot with grammY. As soon as the user
-     * > starts your bot, you will receive `custom-payload` in the `ctx.match`
+     * > (https://core.telegram.org/bots/features#deep-linking) to let users start your
+     * > bot with a custom payload. As an example, send someone the link
+     * > https://t.me/name-of-your-bot?start=custom-payload and register a start
+     * > command handler on your bot with grammY. As soon as the user starts
+     * > your bot, you will receive `custom-payload` in the `ctx.match`
      * > property!
      * > ```ts
      * > bot.command('start', ctx => {
@@ -367,8 +367,8 @@ export class Composer<C extends Context> implements MiddlewareObj<C> {
      * // etc
      * ```
      *
-     * If you need more freedom matching your commands, check out the `commands`
-     * plugin.
+     * If you need more freedom matching your commands, check out the
+     * `command-filter` plugin.
      *
      * @param command The command to look for
      * @param middleware The middleware to register
