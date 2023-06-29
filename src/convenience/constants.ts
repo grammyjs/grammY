b4a7d41f39fa4611726aad1f8a2fc229431428e5
Rogério Munhoz <rogerio.munhoz@pipefy.com>
2023-06-29 12:28:30 -0300

    chore: freeze api constants after exporting

--- src/convenience/constants.ts
+++ src/convenience/constants.ts
@@ -30,55 +30,55 @@ export interface ApiConstants {
 /**
  * Contains lists of constants which are useful when working with the Bot API.
  */
-export const API_CONSTANTS = Object.freeze(
-    {
-        /**
-         * List of update types a bot receives by default. Useful if you want to
-         * receive all update types but `chat_member`.
-         *
-         * ```ts
-         * // Built-in polling:
-         * bot.start({ allowed_updates: DEFAULT_UPDATE_TYPES });
-         * // grammY runner:
-         * run(bot, { runner: { fetch: { allowed_updates: DEFAULT_UPDATE_TYPES } } });
-         * // Webhooks:
-         * await bot.api.setWebhook(url, { allowed_updates: DEFAULT_UPDATE_TYPES });
-         * ```
-         */
-        DEFAULT_UPDATE_TYPES,
-        /**
-         * List of all available update types. Useful if you want to receive all
-         * updates from the Bot API, rather than just those that are delivered by
-         * default.
-         *
-         * The main use case for this is when you want to receive `chat_member`
-         * updates, as they need to be enabled first. Use it like so:
-         *
-         * ```ts
-         * // Built-in polling:
-         * bot.start({ allowed_updates: ALL_UPDATE_TYPES });
-         * // grammY runner:
-         * run(bot, { runner: { fetch: { allowed_updates: ALL_UPDATE_TYPES } } });
-         * // Webhooks:
-         * await bot.api.setWebhook(url, { allowed_updates: ALL_UPDATE_TYPES });
-         * ```
-         */
-        ALL_UPDATE_TYPES,
-        /**
-         * An object containing all available chat permissions. Useful if you want
-         * to lift restrictions from a user, as this action requires you to pass
-         * `true` for all permissions. Use it like so:
-         *
-         * ```ts
-         * // On `Bot`:
-         * await bot.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
-         * // On `Api`:
-         * await ctx.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
-         * // On `Context`:
-         * await ctx.restrictChatMember(user_id, ALL_CHAT_PERMISSIONS);
-         * await ctx.restrictAuthor(ALL_CHAT_PERMISSIONS);
-         * ```
-         */
-        ALL_CHAT_PERMISSIONS,
-    } as const,
-);
+export const API_CONSTANTS = {
+    /**
+     * List of update types a bot receives by default. Useful if you want to
+     * receive all update types but `chat_member`.
+     *
+     * ```ts
+     * // Built-in polling:
+     * bot.start({ allowed_updates: DEFAULT_UPDATE_TYPES });
+     * // grammY runner:
+     * run(bot, { runner: { fetch: { allowed_updates: DEFAULT_UPDATE_TYPES } } });
+     * // Webhooks:
+     * await bot.api.setWebhook(url, { allowed_updates: DEFAULT_UPDATE_TYPES });
+     * ```
+     */
+    DEFAULT_UPDATE_TYPES,
+    /**
+     * List of all available update types. Useful if you want to receive all
+     * updates from the Bot API, rather than just those that are delivered by
+     * default.
+     *
+     * The main use case for this is when you want to receive `chat_member`
+     * updates, as they need to be enabled first. Use it like so:
+     *
+     * ```ts
+     * // Built-in polling:
+     * bot.start({ allowed_updates: ALL_UPDATE_TYPES });
+     * // grammY runner:
+     * run(bot, { runner: { fetch: { allowed_updates: ALL_UPDATE_TYPES } } });
+     * // Webhooks:
+     * await bot.api.setWebhook(url, { allowed_updates: ALL_UPDATE_TYPES });
+     * ```
+     */
+    ALL_UPDATE_TYPES,
+    /**
+     * An object containing all available chat permissions. Useful if you want
+     * to lift restrictions from a user, as this action requires you to pass
+     * `true` for all permissions. Use it like so:
+     *
+     * ```ts
+     * // On `Bot`:
+     * await bot.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
+     * // On `Api`:
+     * await ctx.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
+     * // On `Context`:
+     * await ctx.restrictChatMember(user_id, ALL_CHAT_PERMISSIONS);
+     * await ctx.restrictAuthor(ALL_CHAT_PERMISSIONS);
+     * ```
+     */
+    ALL_CHAT_PERMISSIONS,
+} as const;
+
+Object.freeze(API_CONSTANTS);
