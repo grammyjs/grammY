ffb15d89963f1f0340daae1d0909cf8f1b5869f6
Rogério Munhoz <rogerio.munhoz@pipefy.com>
2023-06-29 10:28:45 -0300

    feat: add types and improved docs for API_CONSTANTS
    export the `DEFAULT_UPDATE_TYPES` constant and the `ApiConstants`
    type from the `src/convenience/constants.ts`

--- src/convenience/constants.ts
+++ src/convenience/constants.ts
@@ -18,44 +18,84 @@ const ALL_CHAT_PERMISSIONS = {
     can_manage_topics: true,
 } as const;
 
+export interface ApiConstants {
+    /**
+     * List of update types a bot receives by default. Useful if you want to
+     * receive all update types but `chat_member`.
+     *
+     * ```ts
+     * // Built-in polling:
+     * bot.start({ allowed_updates: DEFAULT_UPDATE_TYPES });
+     * // grammY runner:
+     * run(bot, { runner: { fetch: { allowed_updates: DEFAULT_UPDATE_TYPES } } });
+     * // Webhooks:
+     * await bot.api.setWebhook(url, { allowed_updates: DEFAULT_UPDATE_TYPES });
+     * ```
+     *
+     * See the [Bot API reference](https://core.telegram.org/bots/api#update)
+     * for more information.
+     */
+    DEFAULT_UPDATE_TYPES: typeof DEFAULT_UPDATE_TYPES[number];
+
+    /**
+     * List of all available update types. Useful if you want to receive all
+     * updates from the Bot API, rather than just those that are delivered by
+     * default.
+     *
+     * The main use case for this is when you want to receive `chat_member`
+     * updates, as they need to be enabled first. Use it like so:
+     *
+     * ```ts
+     * // Built-in polling:
+     * bot.start({ allowed_updates: ALL_UPDATE_TYPES });
+     * // grammY runner:
+     * run(bot, { runner: { fetch: { allowed_updates: ALL_UPDATE_TYPES } } });
+     * // Webhooks:
+     * await bot.api.setWebhook(url, { allowed_updates: ALL_UPDATE_TYPES });
+     * ```
+     *
+     * See the [Bot API reference](https://core.telegram.org/bots/api#update)
+     * for more information.
+     */
+    ALL_UPDATE_TYPES: typeof ALL_UPDATE_TYPES[number];
+
+    /**
+     * An object containing all available chat permissions. Useful if you want
+     * to lift restrictions from a user, as this action requires you to pass
+     * `true` for all permissions. Use it like so:
+     *
+     * ```ts
+     * // On `Bot`:
+     * await bot.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
+     * // On `Api`:
+     * await ctx.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
+     * // On `Context`:
+     * await ctx.restrictChatMember(user_id, ALL_CHAT_PERMISSIONS);
+     * await ctx.restrictAuthor(ALL_CHAT_PERMISSIONS);
+     * ```
+     *
+     * See the [Bot API reference](https://core.telegram.org/bots/api#update)
+     * for more information.
+     */
+    ALL_CHAT_PERMISSIONS: keyof typeof ALL_CHAT_PERMISSIONS;
+}
+
+interface TypeOf {
+    DEFAULT_UPDATE_TYPES: typeof DEFAULT_UPDATE_TYPES;
+    ALL_UPDATE_TYPES: typeof ALL_UPDATE_TYPES;
+    ALL_CHAT_PERMISSIONS: typeof ALL_CHAT_PERMISSIONS;
+}
+type ValuesFor<T> = {
+    [K in keyof T]: K extends keyof TypeOf ? Readonly<TypeOf[K]> : never;
+};
+
 /**
- * Contains lists of constants which are useful when working with the Bot API.
+ * A container for constants used in the Telegram Bot API. Currently holds all
+ * available update types as well as all chat permissions.
  */
-export const API_CONSTANTS = Object.freeze(
-    {
-        /**
-         * List of all available update types. Useful if you want to receive all
-         * updates from the Bot API, rather than just those that are delivered by
-         * default.
-         *
-         * The main use case for this is when you want to receive `chat_member`
-         * updates, as they need to be enabled first. Use it like so:
-         *
-         * ```ts
-         * // Built-in polling:
-         * bot.start({ allowed_updates: ALL_UPDATE_TYPES });
-         * // grammY runner:
-         * run(bot, { runner: { fetch: { allowed_updates: ALL_UPDATE_TYPES } } });
-         * // Webhooks:
-         * await bot.api.setWebhook(url, { allowed_updates: ALL_UPDATE_TYPES });
-         * ```
-         */
-        ALL_UPDATE_TYPES,
-        /**
-         * An object containing all available chat permissions. Useful if you want
-         * to lift restrictions from a user, as this action requires you to pass
-         * `true` for all permissions. Use it like so:
-         *
-         * ```ts
-         * // On `Bot`:
-         * await bot.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
-         * // On `Api`:
-         * await ctx.api.restrictChatMember(chat_id, user_id, ALL_CHAT_PERMISSIONS);
-         * // On `Context`:
-         * await ctx.restrictChatMember(user_id, ALL_CHAT_PERMISSIONS);
-         * await ctx.restrictAuthor(ALL_CHAT_PERMISSIONS);
-         * ```
-         */
-        ALL_CHAT_PERMISSIONS,
-    } as const,
-);
+export const API_CONSTANTS: ValuesFor<ApiConstants> = {
+    DEFAULT_UPDATE_TYPES,
+    ALL_UPDATE_TYPES,
+    ALL_CHAT_PERMISSIONS,
+};
+Object.freeze(API_CONSTANTS);
