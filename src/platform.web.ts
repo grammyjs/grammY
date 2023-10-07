25f5e2bea6bbf1c70ed41542a50c794a08a53096
KnightNiwrem <merwinwee@gmail.com>
2023-10-08 01:38:44 +0800

    fix(web): do not rely on ReadableStream.from or new ReadableStream

--- src/platform.web.ts
+++ src/platform.web.ts
@@ -2,17 +2,27 @@ import d from "https://cdn.skypack.dev/debug@4.3.4";
 export { d as debug };
 
 // === Export system-specific operations
+async function writeAsyncItrToStream<T>(
+    itr: AsyncIterable<T>,
+    writer: WritableStreamDefaultWriter<T>,
+) {
+    for await (const chunk of itr) {
+        await writer.write(chunk);
+    }
+    await writer.close();
+}
+
 // Turn an AsyncIterable<Uint8Array> into a stream
 export const itrToStream = (itr: AsyncIterable<Uint8Array>) => {
-    // do not assume ReadableStream.from to exist yet
-    const it = itr[Symbol.asyncIterator]();
-    return new ReadableStream({
-        async pull(controller) {
-            const chunk = await it.next();
-            if (chunk.done) controller.close();
-            else controller.enqueue(chunk.value);
-        },
-    });
+    // do not assume ReadableStream.from or new ReadableStream to exist yet
+    const { readable, writable } = new TransformStream();
+    const writer = writable.getWriter();
+
+    writeAsyncItrToStream(itr, writer).catch((err) =>
+        console.error("Something went wrong with writing itrToStream")
+    );
+
+    return readable;
 };
 
 // === Base configuration for `fetch` calls
