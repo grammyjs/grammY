0681f040fdda839e6a2e036ea9581b9e562ea42a
KnorpelSenf <shtrog@gmail.com>
2023-12-30 15:39:10 +0100

    build: update deps

--- src/types.web.ts
+++ src/types.web.ts
@@ -1,5 +1,5 @@
 // === Needed imports
-import { basename } from "https://deno.land/std@0.202.0/path/basename.ts";
+import { basename } from "https://deno.land/std@0.210.0/path/basename.ts";
 import {
     type ApiMethods as ApiMethodsF,
     type InputMedia as InputMediaF,
@@ -10,10 +10,10 @@ import {
     type InputMediaVideo as InputMediaVideoF,
     type InputSticker as InputStickerF,
     type Opts as OptsF,
-} from "https://deno.land/x/grammy_types@v3.4.1/mod.ts";
+} from "https://deno.land/x/grammy_types@v3.4.2/mod.ts";
 
 // === Export all API types
-export * from "https://deno.land/x/grammy_types@v3.4.1/mod.ts";
+export * from "https://deno.land/x/grammy_types@v3.4.2/mod.ts";
 
 /** Something that looks like a URL. */
 interface URLLike {
