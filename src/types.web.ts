4cb3d496e15442ebb4ca7a1cc270c752471170df
KnorpelSenf <shtrog@gmail.com>
2024-04-01 22:31:56 +0200

    build: update web build deps

--- src/types.web.ts
+++ src/types.web.ts
@@ -10,10 +10,10 @@ import {
     type InputMediaVideo as InputMediaVideoF,
     type InputSticker as InputStickerF,
     type Opts as OptsF,
-} from "https://deno.land/x/grammy_types@v3.5.2/mod.ts";
+} from "https://deno.land/x/grammy_types@v3.6.0/mod.ts";
 
 // === Export all API types
-export * from "https://deno.land/x/grammy_types@v3.5.2/mod.ts";
+export * from "https://deno.land/x/grammy_types@v3.6.0/mod.ts";
 
 /** Something that looks like a URL. */
 interface URLLike {
