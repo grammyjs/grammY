7c5ffc47ba67533583f61423f526c9630d3911d1
KnorpelSenf <shtrog@gmail.com>
2024-10-27 14:15:11 +0100

    test: reduce indentation of test

--- test/context.type.test.ts
+++ test/context.type.test.ts
@@ -7,47 +7,44 @@ import {
     it,
 } from "./deps.test.ts";
 
-describe(
-    "ctx.has* checks",
-    () => {
-        it("should narrow down types", () => {
-            const c = new Composer<Context & { state: 1 }>();
-            c.use((ctx) => {
+describe("ctx.has* checks", () => {
+    it("should narrow down types", () => {
+        const c = new Composer<Context & { state: 1 }>();
+        c.use((ctx) => {
+            assertType<IsExact<typeof ctx.state, 1>>(true);
+            if (ctx.has(":contact")) {
+                assertType<
+                    IsExact<typeof ctx.msg.contact.phone_number, string>
+                >(true);
                 assertType<IsExact<typeof ctx.state, 1>>(true);
-                if (ctx.has(":contact")) {
-                    assertType<
-                        IsExact<typeof ctx.msg.contact.phone_number, string>
-                    >(true);
-                    assertType<IsExact<typeof ctx.state, 1>>(true);
-                }
-                if (ctx.hasText("123")) {
-                    assertType<
-                        Has<typeof ctx.match, string | RegExpMatchArray>
-                    >(true);
-                }
-                if (ctx.hasCommand("123")) {
-                    assertType<Has<typeof ctx.match, string>>(true);
-                }
-                if (ctx.hasChatType("private")) {
-                    assertType<IsExact<typeof ctx.chat.type, "private">>(true);
-                }
-                if (ctx.hasGameQuery("123")) {
-                    assertType<
-                        IsExact<
-                            typeof ctx.callbackQuery.game_short_name,
-                            string
-                        >
-                    >(true);
-                }
-                if (ctx.hasInlineQuery("123")) {
-                    assertType<IsExact<typeof ctx.inlineQuery.id, string>>(
-                        true,
-                    );
-                }
-            });
-            c.command("c", (ctx) => {
+            }
+            if (ctx.hasText("123")) {
+                assertType<
+                    Has<typeof ctx.match, string | RegExpMatchArray>
+                >(true);
+            }
+            if (ctx.hasCommand("123")) {
                 assertType<Has<typeof ctx.match, string>>(true);
-            });
+            }
+            if (ctx.hasChatType("private")) {
+                assertType<IsExact<typeof ctx.chat.type, "private">>(true);
+            }
+            if (ctx.hasGameQuery("123")) {
+                assertType<
+                    IsExact<
+                        typeof ctx.callbackQuery.game_short_name,
+                        string
+                    >
+                >(true);
+            }
+            if (ctx.hasInlineQuery("123")) {
+                assertType<IsExact<typeof ctx.inlineQuery.id, string>>(
+                    true,
+                );
+            }
         });
-    },
-);
+        c.command("c", (ctx) => {
+            assertType<Has<typeof ctx.match, string>>(true);
+        });
+    });
+});
