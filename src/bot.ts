5aac3e654b164eb89aac6aaa77d3634a3e35f12a
KnorpelSenf <shtrog@gmail.com>
2024-10-27 13:25:41 +0100

    docs: update PollingOptions.allowed_updates (#650)

--- src/bot.ts
+++ src/bot.ts
@@ -63,11 +63,11 @@ export interface PollingOptions {
     timeout?: number;
     /**
      * A list of the update types you want your bot to receive. For example,
-     * specify [“message”, “edited_channel_post”, “callback_query”] to only
+     * specify ["message", "edited_channel_post", "callback_query"] to only
      * receive updates of these types. See Update for a complete list of
      * available update types. Specify an empty list to receive all update types
-     * except chat_member (default). If not specified, the previous setting will
-     * be used.
+     * except chat_member, message_reaction, and message_reaction_count
+     * (default). If not specified, the previous setting will be used.
      *
      * Please note that this parameter doesn't affect updates created before the
      * call to the getUpdates, so unwanted updates may be received for a short
