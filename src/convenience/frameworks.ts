9d73f17b5344160958c67d5ba900a040237658ee
Thada Wangthammang <thada.wth@gmail.com>
2025-01-16 19:03:47 +0700

    type: fix Azure Function v4 Adapter HttpRequest and Context Type

--- src/convenience/frameworks.ts
+++ src/convenience/frameworks.ts
@@ -85,13 +85,23 @@ export type AzureAdapter = (context: {
         [key: string]: any;
     };
 }, request: { body?: unknown }) => ReqResHandler;
-export type AzureAdapterV4 = (request: Request, context: {
-    res?: {
-        status: number;
-        body: string;
-        headers?: Record<string, string>;
-    };
-}) => ReqResHandler<{ status: number; body?: string } | { jsonBody: string }>;
+export type AzureV4HttpRequest = {
+    method: string;
+    url: string;
+    headers: any;
+    query: URLSearchParams;
+    body: any;
+}
+export type AzureV4InvocationContext = {
+    invocationId: string;
+    functionName: string;
+}
+export type AzureAdapterV4 = (request: AzureV4HttpRequest, context: AzureV4InvocationContext) => ReqResHandler<{
+    status: number;
+    body?: string;
+} | {
+    jsonBody: string;
+}>;
 
 export type BunAdapter = (request: {
     headers: Headers;
