c014ce8ffecaef4903cd12458719285fbc2c283e
Roz <roz@rjmunhoz.me>
2025-03-14 23:59:39 -0300

    chore: remove sessions plugin

--- src/mod.ts
+++ src/mod.ts
@@ -1,23 +1,23 @@
 // Commonly used stuff
 export {
-    Bot,
-    type BotConfig,
-    BotError,
-    type ErrorHandler,
-    type PollingOptions,
+  Bot,
+  type BotConfig,
+  BotError,
+  type ErrorHandler,
+  type PollingOptions,
 } from "./bot.ts";
 
 export { InputFile } from "./types.ts";
 
 export {
-    type CallbackQueryContext,
-    type ChatTypeContext,
-    type CommandContext,
-    Context,
-    type GameQueryContext,
-    type HearsContext,
-    type InlineQueryContext,
-    type ReactionContext,
+  type CallbackQueryContext,
+  type ChatTypeContext,
+  type CommandContext,
+  Context,
+  type GameQueryContext,
+  type HearsContext,
+  type InlineQueryContext,
+  type ReactionContext,
 } from "./context.ts";
 
 // Convenience stuff, built-in plugins, and helpers
@@ -25,39 +25,38 @@ export * from "./convenience/constants.ts";
 export * from "./convenience/inline_query.ts";
 export * from "./convenience/input_media.ts";
 export * from "./convenience/keyboard.ts";
-export * from "./convenience/session.ts";
 export * from "./convenience/webhook.ts";
 
 // A little more advanced stuff
 export {
-    type CallbackQueryMiddleware,
-    type ChatTypeMiddleware,
-    type CommandMiddleware,
-    Composer,
-    type GameQueryMiddleware,
-    type HearsMiddleware,
-    type InlineQueryMiddleware,
-    type Middleware,
-    type MiddlewareFn,
-    type MiddlewareObj,
-    type NextFunction,
-    type ReactionMiddleware,
+  type CallbackQueryMiddleware,
+  type ChatTypeMiddleware,
+  type CommandMiddleware,
+  Composer,
+  type GameQueryMiddleware,
+  type HearsMiddleware,
+  type InlineQueryMiddleware,
+  type Middleware,
+  type MiddlewareFn,
+  type MiddlewareObj,
+  type NextFunction,
+  type ReactionMiddleware,
 } from "./composer.ts";
 
 export {
-    type FilterQuery,
-    type FilterQueryContext,
-    matchFilter,
+  type FilterQuery,
+  type FilterQueryContext,
+  matchFilter,
 } from "./filter.ts";
 
 // Internal stuff for expert users
 export { Api } from "./core/api.ts";
 export {
-    type ApiCallFn,
-    type ApiClientOptions,
-    type RawApi,
-    type TransformableApi,
-    type Transformer,
-    type WebhookReplyEnvelope,
+  type ApiCallFn,
+  type ApiClientOptions,
+  type RawApi,
+  type TransformableApi,
+  type Transformer,
+  type WebhookReplyEnvelope,
 } from "./core/client.ts";
 export { GrammyError, HttpError } from "./core/error.ts";
