c17715bb1752161302287dea0079b8795dcc79c3
Roz <3948961+roziscoding@users.noreply.github.com>
2025-03-17 11:57:51 -0300

    chore: remove sessions plugin (#773)
    The plugin was moved to https://github.com/grammyjs/sessions.

--- src/mod.ts
+++ src/mod.ts
@@ -25,7 +25,6 @@ export * from "./convenience/constants.ts";
 export * from "./convenience/inline_query.ts";
 export * from "./convenience/input_media.ts";
 export * from "./convenience/keyboard.ts";
-export * from "./convenience/session.ts";
 export * from "./convenience/webhook.ts";
 
 // A little more advanced stuff
