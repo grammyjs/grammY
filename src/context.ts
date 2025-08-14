a8ba76b35d275c31fc95e9a98ed1be7501701c4f
KnorpelSenf <shtrog@gmail.com>
2025-08-14 19:36:18 +0200

    docs: fix missing param for unpinChatMessage

--- src/context.ts
+++ src/context.ts
@@ -2308,6 +2308,7 @@ export class Context implements RenamedUpdate {
      * Context-aware alias for `api.unpinChatMessage`. Use this method to remove a message from the list of pinned messages in a chat. If the chat is not a private chat, the bot must be an administrator in the chat for this to work and must have the 'can_pin_messages' administrator right in a supergroup or 'can_edit_messages' administrator right in a channel. Returns True on success.
      *
      * @param message_id Identifier of a message to unpin. If not specified, the most recent pinned message (by sending date) will be unpinned.
+     * @param other Optional remaining parameters, confer the official reference below
      * @param signal Optional `AbortSignal` to cancel the request
      *
      * **Official reference:** https://core.telegram.org/bots/api#unpinchatmessage
