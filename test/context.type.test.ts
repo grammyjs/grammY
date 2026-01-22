9b8b31bfa9e78ea56a0a1d0cfab6c5193f7e8685
KnorpelSenf <shtrog@gmail.com>
2026-01-22 20:04:05 +0100

    test: cover types of combining composer methods and has checks

--- test/context.type.test.ts
+++ test/context.type.test.ts
@@ -1,5 +1,11 @@
 import { Composer, type Context } from "../src/mod.ts";
-import { assertType, describe, type IsExact, it } from "./deps.test.ts";
+import {
+    assertType,
+    describe,
+    type IsExact,
+    type IsNullable,
+    it,
+} from "./deps.test.ts";
 
 describe("ctx.has* checks", () => {
     it("should narrow down types", () => {
@@ -38,5 +44,37 @@ describe("ctx.has* checks", () => {
         c.command("c", (ctx) => {
             assertType<IsExact<typeof ctx.args, string>>(true);
         });
+    });
+    it("should narrow down types in combination with middleware narrowing", () => {
+        const c = new Composer<Context & { state: 1 }>();
+        c.on(["deleted_business_messages", "callback_query"])
+            .callbackQuery("dummy", (ctx) => {
+                if (ctx.hasChatType("private")) {
+                    const update = ctx.update;
+                    const upCbqueryChatType = ctx.update.callback_query.message
+                        ?.chat.type;
+                    const chatType = ctx.chat.type;
+                    const chatId = ctx.chatId;
+                    const from = ctx.from.id;
+                    const fromId = ctx.fromId;
+                    const msgChatType = ctx.msg?.chat.type;
+                    const cbqueryChatType = ctx.callbackQuery.message?.chat
+                        .type;
+                    assertType<IsNullable<typeof update>>(false);
+                    assertType<
+                        IsExact<typeof upCbqueryChatType, "private" | undefined>
+                    >(true);
+                    assertType<IsExact<typeof chatType, "private">>(true);
+                    assertType<IsExact<typeof chatId, number>>(true);
+                    assertType<IsExact<typeof from, number>>(true);
+                    assertType<IsExact<typeof fromId, number>>(true);
+                    assertType<
+                        IsExact<typeof msgChatType, "private" | undefined>
+                    >(true);
+                    assertType<
+                        IsExact<typeof cbqueryChatType, "private" | undefined>
+                    >(true);
+                }
+            });
     });
 });
