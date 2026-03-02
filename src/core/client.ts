05857b3f068a5556ef39a211c7cc0509a0ab9bf5
KnorpelSenf <shtrog@gmail.com>
2026-03-02 11:04:37 +0100

    fix: throw meaningful error for non-JSON bodies

--- src/core/client.ts
+++ src/core/client.ts
@@ -317,7 +317,7 @@ class ApiClient<R extends RawApi> {
         const options = { ...opts.baseFetchConfig, signal: sig, ...config };
         // Perform fetch call
         const successPromise = this.fetch(url, options)
-            .then((res) => res.json());
+            .then(parseApiResponseBody<R, M>);
         // Those are the three possible outcomes of the fetch call:
         const operations = [successPromise, streamErr.promise, timeout.promise];
         // Wait for result
@@ -424,6 +424,19 @@ const proxyMethods = {
         return [];
     },
 };
+
+async function parseApiResponseBody<R extends RawApi, M extends Methods<R>>(
+    res: Response,
+) {
+    if (res.headers.get("content-type") !== "application/json") {
+        throw new Error(
+            `response did not contain a JSON body (${res.status}: ${res.statusText})`,
+            { cause: res },
+        );
+    }
+    const apiResponse: ApiResponse<ApiCallResult<M, R>> = await res.json();
+    return apiResponse;
+}
 
 /** A container for a rejecting promise */
 interface AsyncError {
