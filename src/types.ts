b64788f17438dba22f8a5c815d98dc4fc36c1b76
KnorpelSenf <shtrog@gmail.com>
2026-07-29 23:20:20 +0200

    docs(types): add missing docs for ApiMethods wrapper

--- src/types.ts
+++ src/types.ts
@@ -153,6 +153,9 @@ export interface Update {
    */
   subscription?: BotSubscriptionUpdated;
 }
+/**
+ * Wrapper type to bundle all methods of the Telegram Bot API.
+ */
 export interface ApiMethods {
   /**
    * Use this method to receive incoming updates using long polling (<a href="https://en.wikipedia.org/wiki/Push_technology#Long_polling">wiki</a>). Returns an Array of <a href="#update">Update</a> objects.
