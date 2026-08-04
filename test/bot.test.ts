79152a442617577e3289b88c2f3c478715626093
KnorpelSenf <shtrog@gmail.com>
2026-08-04 19:26:36 +0200

    test: include new properties in UserFromGetMe

--- test/bot.test.ts
+++ test/bot.test.ts
@@ -29,9 +29,14 @@ const me: UserFromGetMe = {
     username: "test_bot",
     can_join_groups: true,
     can_read_all_group_messages: false,
+    supports_guest_queries: false,
     supports_inline_queries: false,
     can_connect_to_business: false,
     has_main_web_app: false,
+    has_topics_enabled: false,
+    allows_users_to_create_topics: false,
+    can_manage_bots: false,
+    supports_join_request_queries: false,
 };
 
 const testUpdate: Update = {
